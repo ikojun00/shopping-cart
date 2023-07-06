feed877cce0a30b03a3eb1953562dacbc0054ff6
ikojun00 <ikojun00@fesb.hr>
2023-07-06 23:36:24 +0200

    features: shopping page with pictures and styling

--- src/CartItem.js
+++ src/CartItem.js
@@ -6,7 +6,7 @@ const CartItem = ({ item, onRemove }) => {
       <img src={item.image} alt={item.name} />
       <div>
         <h4>{item.name}</h4>
-        <p>Price: ${item.price}</p>
+        <p>Price: €{item.price}</p>
         <p>Quantity: {item.quantity}</p>
       </div>
       <button onClick={() => onRemove(item)}>Remove</button>
--- src/ShoppingPage.js
+++ src/ShoppingPage.js
@@ -1,20 +1,29 @@
 import React from 'react';
-import Cart from './Cart';
+import banana from './images/banana.png';
+import apple from './images/apple.png';
+import milk from './images/milk.png';
+import bread from './images/bread.png';
+import orange from './images/orange.png';
 
 const ShoppingPage = () => {
   const items = [
-    { id: 1, name: 'Item 1', price: 10, quantity: 2, image: 'item1.jpg' },
-    { id: 2, name: 'Item 2', price: 20, quantity: 1, image: 'item2.jpg' },
+    { id: 1, name: 'Banana', price: '1.5€', image: banana },
+    { id: 2, name: 'Apple', price: '1.2€', image: apple },
+    { id: 3, name: 'Bread', price: '1.3€', image: bread },
+    { id: 4, name: 'Milk', price: '1.5€', image: milk },
+    { id: 5, name: 'Orange', price: '1.5€', image: orange },
   ];
 
-  const handleRemoveFromCart = (item) => {
-    // Implement logic to remove item from the cart
-  };
-
   return (
-    <div>
-      <h2>Shopping Page</h2>
-      <Cart items={items} onRemove={handleRemoveFromCart} />
+    <div className='shoppingPage'>
+      {items.map((item) => (
+        <div className='card' key={item.id}>
+          <img src={item.image} alt={item.name} />
+          <p>{item.name}</p>
+          <p>{item.price}</p>
+          <button>Add to cart</button>
+        </div>
+      ))}
     </div>
   );
 };
