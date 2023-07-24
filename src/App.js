726c744d2c84c9a8f9bc53e3e67aefa92befb97d
ikojun00 <ikojun00@fesb.hr>
2023-07-24 15:04:02 +0200

    preloading images

--- src/App.js
+++ src/App.js
@@ -4,9 +4,21 @@ import HomePage from './HomePage';
 import ShoppingPage from './ShoppingPage';
 import Cart from './Cart';
 
+import banana from './images/banana.png';
+import apple from './images/apple.png';
+import milk from './images/milk.png';
+import bread from './images/bread.png';
+import orange from './images/orange.png';
+
 const App = () => {
   const [cartItems, setCartItems] = useState([]);
+  const [cartImages, setCartImages] = useState([]);
   const [sumOfItems, setSumOfItems] = useState(0);
+
+  useEffect(() => {
+    const imgs = [banana, apple, milk, bread, orange];
+    cacheImages(imgs)
+  }, []);
 
   useEffect(() => {
     let sum = 0;
@@ -15,6 +27,19 @@ const App = () => {
     });
     setSumOfItems(sum);
   }, [cartItems]);
+
+  const cacheImages = async(srcArray) => {
+    const promises = await srcArray.map((src) => {
+      return new Promise (function (resolve, reject) {
+        const img = new Image();
+        img.src = src;
+        img.onload = resolve();
+        img.onerror = reject();
+      });
+    });
+    await Promise.all(promises);
+    setCartImages(srcArray);
+  }
 
   const handleAddToCart = (item) => {
     const existingItemIndex = cartItems.findIndex((cartItem) => cartItem.id === item.id);
@@ -61,7 +86,7 @@ const App = () => {
 
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/shopping" element={<ShoppingPage handleAddToCart={handleAddToCart} />} />
+        <Route path="/shopping" element={<ShoppingPage handleAddToCart={handleAddToCart} cartImages={cartImages}/>} />
         <Route path="/cart" element={<Cart items={cartItems} handleRemoveCartItem={handleRemoveCartItem} 
         handleCartItemSubtraction={handleCartItemSubtraction} handleAddToCart={handleAddToCart}/>} />
       </Routes>
--- src/ShoppingPage.js
+++ src/ShoppingPage.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import banana from './images/banana.png';
-import apple from './images/apple.png';
-import milk from './images/milk.png';
-import bread from './images/bread.png';
-import orange from './images/orange.png';
 
-const ShoppingPage = ({ handleAddToCart }) => {
+const ShoppingPage = ({ handleAddToCart, cartImages }) => {
   const items = [
-    { id: 1, name: 'Banana', price: 1.5, quantity: 1, image: banana },
-    { id: 2, name: 'Apple', price: 1.2, quantity: 1, image: apple },
-    { id: 3, name: 'Bread', price: 1.3, quantity: 1, image: bread },
-    { id: 4, name: 'Milk', price: 1.5, quantity: 1, image: milk },
-    { id: 5, name: 'Orange', price: 1.5, quantity: 1, image: orange },
+    { id: 1, name: 'Banana', price: 1.5, quantity: 1, image: cartImages[0] },
+    { id: 2, name: 'Apple', price: 1.2, quantity: 1, image: cartImages[1] },
+    { id: 3, name: 'Bread', price: 1.3, quantity: 1, image: cartImages[2] },
+    { id: 4, name: 'Milk', price: 1.5, quantity: 1, image: cartImages[3] },
+    { id: 5, name: 'Orange', price: 1.5, quantity: 1, image: cartImages[4] },
   ];
 
   return (
